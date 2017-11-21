74df72e6dd2892628a173c889f7aa9268de52deb
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2017-11-20 19:05:56 -0500

    Add script to fix TRs

---
+++ scripts/fix_trs_bold.py
@@ -0,0 +1,9 @@
+import nibabel as nib
+import sys
+
+tofix = sys.argv[1]
+img = nib.load(tofix)
+old_zooms = img.header.get_zooms()
+new_zooms = [z for z in old_zooms[:3]] + [2.0]
+img.header.set_zooms(new_zooms)
+img.to_filename(tofix.replace('.nii.gz', '_trfixed.nii.gz')
