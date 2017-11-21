7996415c61f44b642465acf8ef249eaf80b0b89a
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2017-11-20 19:07:36 -0500

    Fix script

--- scripts/fix_trs_bold.py
+++ scripts/fix_trs_bold.py
@@ -7,4 +7,4 @@ img = nib.load(tofix)
 old_zooms = img.header.get_zooms()
 new_zooms = [z for z in old_zooms[:3]] + [2.0]
 img.header.set_zooms(new_zooms)
-img.to_filename(tofix.replace('.nii.gz', '_trfixed.nii.gz')
+img.to_filename(tofix.replace('.nii.gz', '_trfixed.nii.gz'))
